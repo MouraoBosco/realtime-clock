7abf9e6170623bb4c9b02aae017a92517c8f4661
MouraoBosco <mateusmouraobosco@hotmail.com>
2021-02-21 16:27:57 -0300

    aadded function for am-pm system

--- js/script.js
+++ js/script.js
@@ -1,5 +1,10 @@
 var myVar = setInterval(myTimer ,1000);
 function myTimer() {
   var d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+  document.getElementById("clock").innerHTML = d.toLocaleTimeString('pt-Br', {hour12: true})
+/*
+Function for changing between timezones
+d.toLocaleTimeString('en-Us', {12hour: true})
+*/
 }
+
